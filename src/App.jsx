6b98720a4066ef94b9163cedc25fc512c931f8a6
SHOR00 <89482309+SHOR00@users.noreply.github.com>
2023-05-01 13:53:54 +0000

    Lo nuevo

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import ListGroup from 'react-bootstrap/ListGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {useRef, useState} from 'react'
+import {useRef, useState, useEffect} from 'react'
 import './App.css'
+
 function AgregarTarea() {
-  
-  const [tarea, setTarea] = useState("");
-  
-  localStorage.setItem('tareaStorage', tarea);
-  
-  const rStorage =  localStorage.getItem("tareaStorage")
   
   const input = useRef();
   const lista = useRef();
+  
+  const [tarea, setTarea] = useState(localStorage.getItem("memoria"));
+  
+  useEffect(()=>{lista.current.innerHTML = tarea},[])
+  
+  const setLocalStorage = value =>{
+    try {
+       setTarea(JSON.stringify(value))
+      localStorage.setItem('memoria', value)
+    } catch (error) {
+       console.error(error)
+    }
+    
+  }
 
   
   const f =()=>{
     lista.current.innerHTML += `<li key=${input.current.value}>${input.current.value}</li>`
-    input.current.value = ""
-    setTarea(lista.current.innerHTML) 
+    input.current.value = ''
+    setLocalStorage(lista.current.innerHTML) 
   };
   
   return (
@@ -32,7 +41,7 @@ function AgregarTarea() {
           color:'white', 
           background:'black'}}
         onDoubleClick={(e)=>{e.target.remove(e.target)
-                      setTarea(lista.current.innerHTML)}}
+      setLocalStorage(lista.current.innerHTML)}}
         ></ListGroup>
     </>
   );
